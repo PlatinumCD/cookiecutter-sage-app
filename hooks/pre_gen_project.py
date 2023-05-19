aa98737b843d2b1fb566e0b1ec52fa0cdf41e949
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-05-18 19:49:35 -0500

    using name in place of repo. added validation for name.

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,14 +1,14 @@
 import re
 import sys
 
-REPO_REGEX = r'^[a-zA-Z][a-zA-Z0-9-]+$'
+NAME_REGEX = r'^[a-z][a-z0-9-]+$'
 VERSION_REGEX = r'^[0-9]+\.[0-9]+\.[0-9]+$'
 
-repo = '{{ cookiecutter.repo }}'
+name = '{{ cookiecutter.name }}'
 version = '{{ cookiecutter.version }}'
 
-if not re.match(REPO_REGEX, repo):
-    sys.exit("ERROR: Invalid repo! Repo must start with a letter and must only contain letters, numbers and hyphens.")
+if not re.match(NAME_REGEX, name):
+    sys.exit("ERROR: Invalid name! Name must start with a letter and must only consist of lowercase letters, numbers and hyphens.")
 
 if not re.match(VERSION_REGEX, version):
     sys.exit("ERROR: Invalid version! Version must follow x.y.z semvar pattern.")
